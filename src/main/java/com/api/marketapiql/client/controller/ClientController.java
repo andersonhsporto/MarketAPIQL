9cd9c693d2348d91304b1bf9e19fa90820016c77
Anderson Porto <anderson.higo2@gmail.com>
2024-05-23 20:04:31 -0300

    Fix: log configuration

--- src/main/java/com/api/marketapiql/client/controller/ClientController.java
+++ src/main/java/com/api/marketapiql/client/controller/ClientController.java
@@ -5,7 +5,7 @@ import com.api.marketapiql.client.domain.ClientInputDTO;
 import com.api.marketapiql.client.domain.ClientService;
 import com.api.marketapiql.client.domain.Product;
 import lombok.AllArgsConstructor;
-import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -16,26 +16,26 @@ import java.util.Collection;
 
 @Controller
 @AllArgsConstructor
-@Log
+@Log4j2
 public class ClientController {
 
     private final ClientService clientService;
 
     @QueryMapping
     public Client clientById(@Argument Long id) {
-        log.info("Finding client by id: " + id);
+        log.info("Finding client by id: {}", id);
         return clientService.findById(id);
     }
 
     @SchemaMapping
     public Collection<Client> clients(Product product) {
-        log.info("Finding clients for product: " + product);
+        log.info("Finding clients for product: {}", product);
         return clientService.findAllByProductId(product.getId());
     }
 
     @MutationMapping
     public Client createClient(@Argument(name = "client") ClientInputDTO clientInputDTO) {
-        log.info("Creating client: " + clientInputDTO);
+        log.info("Creating client: {}", clientInputDTO);
         return clientService.createClient(clientInputDTO);
     }
 
